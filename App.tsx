9dd61d90225adeb5b94182976622f27407b4aa1e
tonymatheus <tony.chaves14@gmailcom>
2022-11-13 08:33:47 -0300

    Starts project structure

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {Login} from '@screens/Login';
+export const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Login />
+    </View>
+  );
+};
---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@services': './src/services',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@hooks': './src/hooks',
+        },
+      },
+    ],
+  ],
+};
