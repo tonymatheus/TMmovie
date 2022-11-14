357baf4b0b5cdad84ae9cb8e38087d73975c3249
tonymatheus <tony.chaves14@gmailcom>
2022-11-14 09:03:10 -0300

    Add the boilerplate of the home details and movie screen

---
+++ src/screens/Details/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Container, Title} from './styles';
+
+export const Details = () => {
+  return (
+    <Container>
+      <Title>Teste</Title>
+    </Container>
+  );
+};
---
+++ src/screens/Movie/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Container, Title} from './styles';
+
+export function Movie() {
+  return (
+    <Container>
+      <Title>Movie</Title>
+    </Container>
+  );
+}
